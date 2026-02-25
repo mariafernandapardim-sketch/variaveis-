e7149a5c83f69390e9a776cad84c234586edad89
mariafernandapardim-esboço <mariafernandapardim@gmail.com>
2026-02-25 14:30:36 -0300

    segundo-commit

--- Principal.java
+++ Principal.java
@@ -2,19 +2,32 @@ package aula3;
 
 import java.util.Scanner;
 
-public class Principal{
+public class Principal {
 
 	public static void main(String[] args) {
-		double fahrenheit, celsius;
-		
-	
+		double preco, distancia, mediaTotal, litrosGastos, custoTotal;
+
 		Scanner ler = new Scanner(System.in);
-		
-		System.out.println("digite o valor em fahrenheit");
-		fahrenheit = ler.nextDouble();
-		
-		celsius = ((fahrenheit -32) * 5) /9;
-		System.out.println("o valor em celsius é " + celsius);
+
+		System.out.println("digite o preço do combustível: ");
+
+		preco = ler.nextDouble();
+
+		System.out.println("digite a distância percorrida: ");
+
+		distancia = ler.nextDouble();
+
+		System.out.println("digite o consumo médio do seu carro: ");
+
+		mediaTotal = ler.nextDouble();
+
+		litrosGastos = distancia / mediaTotal;
+
+		custoTotal = litrosGastos * preco;
+
+		System.out.println("você gastou " + litrosGastos + "litros" + "com o valor de:" + custoTotal);
+
+		ler.close();
 	}
 
 }
